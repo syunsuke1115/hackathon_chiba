b15abe25531514e7536ee507c518ff03208456aa
“syunsuke1115” <kinopio1115@gmail.com>
2022-07-24 10:18:32 +0900

    home画面を作成

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,17 +1,59 @@
 import React from "react";
 import "./Main";
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom";
+import { Button, Box, Typography } from "@mui/material";
+import "./Home.css";
+import HomeImage from "../img/home_image.svg";
 
 function Home() {
   return (
     <>
-      <div>Home</div>
-      <div className="w-100 text-center mt-2">
-        ログイン <Link to="/login">login</Link>
+      <img src={HomeImage} />
+      <div className="home--message">
+        <h1>Slackへようこそ</h1>
       </div>
-      <div className="w-100 text-center mt-2">
-        サインアップ <Link to="/signup">sign up</Link>
-      </div>
+      <Button
+        variant="outlined"
+        className="home--login"
+        fullWidth
+        component={Link}
+        to="/login"
+      >
+        {" "}
+        ログイン
+      </Button>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          "&::before, &::after": {
+            borderTop: "solid 1px",
+            content: '""',
+            flexGrow: 1,
+          },
+          "&::before": {
+            mr: 1,
+          },
+          "&::after": {
+            ml: 1,
+          },
+        }}
+        className="home--horizontal-line"
+      >
+        <Typography variant="h5" className="home--horizontal-text">
+          あるいは
+        </Typography>
+      </Box>
+      <Button
+        variant="outlined"
+        className="home--siginup"
+        fullWidth
+        component={Link}
+        to="/signup"
+      >
+        {" "}
+        新規登録
+      </Button>
     </>
   );
 }
