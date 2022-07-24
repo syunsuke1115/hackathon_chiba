4703a819829e1612171404087fca7a4e38e3d6dc
“syunsuke1115” <kinopio1115@gmail.com>
2022-07-24 10:40:01 +0900

    sigin up画面のUIを作成

--- front/src/components/Signup.js
+++ front/src/components/Signup.js
@@ -1,16 +1,19 @@
-import React, { useRef, useState } from "react"
-import { Form, Button, Card, Alert } from "react-bootstrap"
-import { Link } from "react-router-dom"
+import React, { useRef, useState } from "react";
+import { Form, Card, Alert } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Button, Box, Typography } from "@mui/material";
+import "./Siginup.css";
 
 function Signup() {
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  const passwordConfirmRef = useRef()
-  const [error, setError] = useState("")
-  const [loading, setLoading] = useState(false)
+  const emailRef = useRef();
+  const nameRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     // if (passwordRef.current.value !== passwordConfirmRef.current.value) {
     //   return setError("Passwords do not match")
@@ -32,29 +35,45 @@ function Signup() {
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
+          <div className="signup--message">
+            <h1>Slackへようこそ</h1>
+          </div>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
+            <Form.Group id="name" className="signup--input-box">
+              <Form.Label>名前</Form.Label>
+              <Form.Control type="name" ref={nameRef} required />
+            </Form.Group>
+            <Form.Group id="email" className="signup--input-box">
+              <Form.Label>メールアドレス</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
             </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
+            <Form.Group id="password" className="signup--input-box">
+              <Form.Label>パスワード</Form.Label>
               <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
+            <Form.Group id="password-confirm" className="signup--input-box">
+              <Form.Label>パスワード（確認用）</Form.Label>
               <Form.Control type="password" ref={passwordConfirmRef} required />
             </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Sign Up
+            <Button
+              variant="outlined"
+              disabled={loading}
+              className="signup--button"
+              fullWidth
+              component={Link}
+              to="/main"
+            >
+              {" "}
+              新規登録
             </Button>
           </Form>
         </Card.Body>
       </Card>
-      <div className="w-100 text-center mt-2">
-        サインアップ（仮） <Link to="/main">sign up</Link>
+      <div className="signup--login-link">
+        <Link to="/login" className="signup--login-text">
+          ログイン
+        </Link>
       </div>
     </>
   );
